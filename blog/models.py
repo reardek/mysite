91d24e1e6687e4af89471f6741483b46104c2d8a
reardek <rebciopl@gmail.com>
2019-09-11 10:03:30 +0200

    adding html

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -28,6 +29,12 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
     class Meta:
         ordering = ('-publish',)
