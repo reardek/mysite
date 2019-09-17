e484382a9e1b6eefcba3743aaa1eea1218a849d6
reardek <rebciopl@gmail.com>
2019-09-17 15:31:34 +0200

    email form

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.data.pl'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dzejdeveloper@data.pl'
+EMAIL_HOST_PASSWORD = 'dzejdeveloper'
+EMAIL_USE_TLS = True
