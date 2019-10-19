e887734b12391f877f6c7047663b100c5058eac5
reardek <47087975+reardek@users.noreply.github.com>
2019-10-19 15:16:08 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': 'campusy12'
+        'PASSWORD': '********'
     }
 }
 
